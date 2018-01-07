866e75f050a4271269095eebaa345bd0d81ada4d
hmoazam <hmoazam@andrew.cmu.edu>
2018-01-07 15:49:18 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,10 +14,17 @@ var yourVideo = document.getElementById("yourVideo");
 var friendsVideo = document.getElementById("friendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
 var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'}]};
-var pc = new RTCPeerConnection(servers);
-pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
-pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+
 var track = null;
+// var pc = bootConnection();
+
+
+function bootConnection() {
+  var pc = new RTCPeerConnection(servers);
+  pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+  pc.onaddstream = (event => friendsVideo.srcObject = event.stream);  
+  return pc;
+}
 
 
 function sendMessage(senderId, data) {
@@ -44,17 +51,29 @@ function readMessage(data) {
 database.on('child_added', readMessage);
 
 function showMyFace() {
-  navigator.mediaDevices.getUserMedia({audio:true, video:true}, function(stream) {
-    track = stream.getTracks()[0];
-  })
-    .then(stream => yourVideo.srcObject = stream)
-    .then(stream => pc.addStream(stream));
+  navigator.mediaDevices.getUserMedia({audio:true, video:true})
+    // .then(stream => yourVideo.srcObject = stream)
+    .then(function(stream){
+      yourVideo.srcObject = stream;
+      track = stream.getTracks()[1];
+      return stream;
+    })
+    // .then(stream => pc.addStream(stream));
+    .then(function(stream){
+      pc = bootConnection();
+      pc.addStream(stream);
+      return stream;
+    })
 }
 
 function showFriendsFace() {
+  // pc = new RTCPeerConnection(servers);
+  // pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+  // pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
   pc.createOffer()
     .then(offer => pc.setLocalDescription(offer) )
     .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
+
 }
 
 function endCall() {
